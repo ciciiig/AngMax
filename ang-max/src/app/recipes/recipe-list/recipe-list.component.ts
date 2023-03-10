ea8c5ab37a68aa236812da7e30c49b84fa953969
George <tsintsadze7@gmail.com>
2023-03-11 02:32:54 +0400

    feat: add recipe list item and some bootstrap 4

--- ang-max/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ang-max/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.interface';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    {
+      name: 'pie',
+      description: 'made with green apples',
+      imagePath: 'https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=face&w=750&h=375&url=https%3A%2F%2Fassets.marthastewart.com%2Fstyles%2Fwmax-750%2Fd30%2Fmile-high-apple-pie-3-xd106255%2Fmile-high-apple-pie-3-xd106255_0.jpg%3Fitok%3DcinyMkFU',
+    }
+  ]
 }
